13e32ab3a296f75bdfc34b9dbd65a24421cc5d84
Shane McDonough <shanemcdman@gmail.com>
2020-04-02 02:05:29 -0700

    Start with rain again

--- sketch.js
+++ sketch.js
@@ -7,8 +7,7 @@ function setup(){
 	createCanvas(window.innerWidth - 20, window.innerHeight - 20);
 	textSize(32);
 	frameRate(35);
-	//createTrails();
-	burst();
+	createTrails();
 }
 
 function createTrails(){
